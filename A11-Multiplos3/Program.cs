036ef2a6ad6204b5aaa59e206a29201ce2292350
Duda Araujo <mduda_araujo@hotmail.com>
2022-09-21 23:45:17 -0300

    Finalização curso 1

---
+++ A11-Multiplos3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Múltiplos de 3");
+
+    
+
+        for(int numero = 1; numero <= 100; numero++)
+        {
+            if(numero % 3 == 0)
+            {
+                Console.WriteLine(numero);
+            }
+
+        }
+       
+
+
+        Console.WriteLine("Tecle para fechar");
+        Console.ReadLine();
+    }
+}
